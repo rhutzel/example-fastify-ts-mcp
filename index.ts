51edf83eee1e18be1d1b301cf833add2f474e89a
Bob Hutzel <amoebob@fastmail.fm>
2025-06-22 20:36:35 -0400

    feat: add tool usage endpoint

--- index.ts
+++ index.ts
@@ -1,6 +1,7 @@
 import { anthropic } from "@ai-sdk/anthropic";
-import { createDataStream, streamText } from 'ai';
+import { streamText, tool } from 'ai';
 import fastify from 'fastify'
+import { z } from 'zod';
 
 const server = fastify({
   logger: true
@@ -11,14 +12,30 @@ server.get('/ping', async (request, reply) => {
 })
 
 server.post('/llm-text', async function (request, reply) {
-  const result = streamText({
+  const result = await streamText({
     maxTokens: 20,
     model: anthropic('claude-3-haiku-20240307'),
     prompt: 'Invent a new holiday and describe its traditions.',
   });
-
   reply.header('Content-Type', 'text/plain; charset=utf-8');
+  return reply.send(result.textStream);
+});
 
+server.post('/tool-example', async function (request, reply) {
+  const result = await streamText({
+    maxSteps: 2,
+    maxTokens: 100,
+    prompt: 'Tell me what my name is.',
+    model: anthropic('claude-3-haiku-20240307'),
+    tools: {
+      name: tool({
+        description: 'Gets the user\'s name',
+        execute: async () => ({ result: 'Johnny' }),
+        parameters: z.object({})
+      })
+    }
+  });
+  reply.header('Content-Type', 'text/plain; charset=utf-8');
   return reply.send(result.textStream);
 });
 
